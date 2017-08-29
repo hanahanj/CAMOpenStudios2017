1c74d01ce6367fbae58b478dfe9f744b925a9bbe
Hanahan <jonathan@milieu.works>
2017-08-29 16:12:25 -0700

    beginning layouts for artist pages

--- js/index.js
+++ js/index.js
@@ -182,10 +182,10 @@ var markers = [
 // Sunday Contents
 var content = [
 ['<div id=bodyContent>'+'<h1>Adrian Cox<?/h1>'+'<h2>Address</h2>'],
-['<div id=bodyContent>'+'<h1>Albert Yowshien Kuo</h1>'],
-['<div id=bodyContent>'+'<h1>Alice Gadel</h1>'],
-['<div id=bodyContent>'+'<h1>Anabun Pottery</h1>'],
-['<div id=bodyContent>'+'<h1>Andy Leicht</h1>'],
+['<div id=bodyContent>'+'<h1>Albert Yowshien Kuo</h1>'+'<h2>Address</h2>'],
+['<div id=bodyContent>'+'<h1>Alice Gadel</h1>'+'<h2>Address</h2>'],
+['<div id=bodyContent>'+'<h1>Anabun Pottery</h1>'+'<h2>Address</h2>'],
+['<div id=bodyContent>'+'<h1>Andy Leicht</h1>'+'<h2>Address</h2>'],
 
 // ['<div id=bodyContent>'+'<h1>Blake Sanders</h1>'],
 // ['<div id=bodyContent>'+'<h1>Brandon Anschultz</h1>'],
@@ -342,91 +342,8 @@ map.fitBounds(bounds);
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-//Menu Move Map Canvas ///////////////////// 
 
 
-
-$('.menuFloatInt').mouseover(function() {
-     $(this).css({"font-style": "italic"});          
-});
-
-
-$('.menuFloatInt').mouseleave('click touchstart', function() {
-     $(this).css({"font-style": "normal"});          
-});
-
-
-
-$('.menuFloatSites').mouseover(function() {
-     $(this).css({"font-style": "italic"});          
-});
-
-
-$('.menuFloatSites').mouseleave('click touchstart', function() {
-     $(this).css({"font-style": "normal"});    
-});
-
-$('.menuFloatAbout').mouseover(function() {
-     $(this).css({"font-style": "italic"});           
-});
-
-
-$('.menuFloatAbout').mouseleave('click touchstart', function() {
-     $(this).css({"font-style": "normal"});  
-});
-
-
-
-$('.menuFloatInt').on('click touchstart', function() { 
-     $('#mapContainer').animate({width: '80%', height: "100%", top: "0%"}, 300); 
-     $('#listItineraries').animate({opacity: '1'}, 800);  
-     $('.about').animate({opacity: '0'}, 300);   
-     $('.logo').animate({opacity: '0'}, 300); 
-     $('#sitesContain').animate({opacity: '0'}, 300);
-     $('.close').animate({opacity: '1'}, 300);
-
-     $(this).css({"text-decoration":"underline", "font-style": "italic"});
-     $('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"}); 
-     $('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});   
-});
-
-$('.menuFloatSites').on('click touchstart', function() {
-     $('#mapContainer').animate({height: '80%', width: "100%", top: '0%'}, 300);  
-     $('#sitesContain').animate({opacity: '1'}, 800);
-     $('.about').animate({opacity: '0'}, 300);   
-     $('.logo').animate({opacity: '0'}, 300); 
-     $('.close').animate({opacity: '1'}, 300);
-
-     $(this).css({"text-decoration":"underline", "font-style": "italic"});
-     $('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});
-     $('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});     
-});
-
-$('.menuFloatAbout').on('click touchstart', function() {
-     $('#mapContainer').animate({height: '75%', width: "100%", top: '25.5%', }), 300;
-     $('#listItineraries').animate({opacity: '0'}, 300);
-     $('.about').animate({opacity: '1'}, 300);   
-     $('.logo').animate({opacity: '1'}, 300); 
-     $('#sitesContain').animate({opacity: '0'}, 300);
-     $('.close').animate({opacity: '1'}, 300);
-
-     $(this).css({"text-decoration":"underline", "font-style": "italic"}); 
-     $('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
-     $('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
-
-});
-
-$('.close').on('click touchstart', function() { 
-     $('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
-     $(this).animate({opacity: '0'}, 300);  
-     $('#listItineraries').animate({opacity: '0'}, 300);
-     $('.about').animate({opacity: '0'}, 300);   
-     $('.logo').animate({opacity: '0'}, 300); 
-     $('#sitesContain').animate({opacity: '0'}, 300);
-     $('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
-     $('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
-     $('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"}); 
-});
 
 
 
